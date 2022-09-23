fdeb9ecfd23af94ce39376f28e90b41adc26a11e
niamatuu <114211933+niamatuu@users.noreply.github.com>
2022-09-23 12:30:11 +0430

    NIAMATU.py
    pkg update
    $ pkg upgrade
    $ pkg install python
    $ pkg install python2
    $ pip install requests
    $ pip install mechanize
    $ pip install bs4
    $ pkg install git
    https://github.com/niamatuu/niamat.git
    ls
    cd niamat
    python niamat.py

---
+++ NIAMATU.py
@@ -0,0 +1,89 @@
+^?ELF^B^A^A^@^@^@^@^@^@^@^@^@^C^@�^@^A^@^@^>
+^@@^@ ^@^^^@^F^@^@^@^D^@^@^@@^@^@^@^@^@^@^@>
+^@^@^R^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^>
+^@^@^R^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^>
+^@^@^R^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^>
+^@^@^R^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^>
+^@^@^Q^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^@^>
+^@^@^P^@^@^@^@^@^@^@c^M^E^@^@^@^B^@y
+^@^@^@^@^@^@^@^@^@^@^A^@^@^@�^@^@^@^A^@^@^@>
+�^@^@^@�qX^\���|BE���nE��^Z�Z�^@^@^@�^@^@^@>
+^@^@^@T^@^@^@~^@^@^@�^@^@^@�^@^@^@1^@^@^@�^>
+^?^@^@^@^@^@^@�,^C^@^@^@^@^@^C^D^@^@^@^@^@^>
+^@^@^@^@^@^@^@^@^@^@^@�M^C^@^@^@^@^@^B^D^@^>
+NIAMATU▼▼▼▼▼▼▼▼◥▼▼▼▼▼▼▼▼▼
+
+ ^[[1;33m∈∈∈∈∈∈∈∈∈∈∈∈∈∈∷⚂ >
+
+ ██   ██♥♥♥♥♥♥♥♥♥♥♥♥
+
+ ^[[1;36m█ⓐⓐⅱⅡ⇡⇡⇡⇡⇡⇡⇡⇡↩←←⇄⇡⇡▼☞☞☞      >
+
+(!)═══════════════════════════════════════
+
+(!) Author   : niamatu
+
+(!) Guthub   : NIAMATUU
+
+(!) Facebook : NIAMATULLAH
+
+(!) Type     : Trail
+
+(!) Enjoy Trail Share Ni in Group ❤
+^[[1;37m(!)════════════════════════════════>
+
+^[[1;37m(!)════════════════════════════════>
+
+^[[1;37m[2] free, Cloning ^[[12;m[M2]
+
+^[[1;37m[0] Exit Menu ^@2^@^[[38;5;208m^@6^>
+
+[•] Total OK Ids : ^@ConnectionError^@D^@Di>
+
+[•] Limint : ^@F^@G^@H^@I^@
+
+(!) Info : Something Working Was Wrong^@J^@>
+
+[•] CP IDS SAVE : /Free/AKINGNIAMATUU-CP.txt^@P^@>
+
+[•] BD Code: 880180,880191,880174,100200,etc...
+
+[•] Code : ^@[•] Press Enter To Back Menu ^>
+
+[•] Total CP IDs : ^@U^@V^@ValueError^@W^@X>
+•] BD Code: 880180,880191,880174,etc...
+
+[•] Code : ^@[•] Press Enter To Back Menu ^>
+
+[•] Total CP IDs : ^@U^@V^@ValueError^@W^@X>
+
+^@^M^@; ^@.^@(^@!^@) ^@  ^@^M ^@[^@a^@aa^@a>
+
+return next yielded value or raise StopIter>
+
+return next yielded value or raise StopIter>
+
+    try: Generator = _module.Generator
+
+    except AttributeError: pass
+
+    else: Generator.register(_cython_genera>
+
+if _cython_coroutine_type is not None:
+
+    try: Coroutine = _module.Coroutine
+
+    except AttributeError: pass
+
+^@need more than %zd value%.1s to unpack^@_>
+^@^@��^@^@
+^@^@x�^@^@@
+^@^@^P�^@^@`
+^@^@��^@^@�
+^@^@8�^@^@�
+^@^@x�^@^@�
+^@^@^T�^@^@�
+^@^@��^@^@^@^K^@^@��^@^@ ^K^@^@��^@^@@^K^@^>
+�^L�^N�^P�^R�^T�^V�^X^@^@^@^\^@^@^@^\^A^@^@>
+�^L^@^\^@^@^@\^A^@^@��^@^@�^@^@^@^@L^L^] �^>
+    else: Coroutine.register(_cython_corout>
